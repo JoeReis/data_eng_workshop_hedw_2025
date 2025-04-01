0c988b1079f6ba525d46bf65f16af2d6f3b00912
Joseph Reis <josephreis@Josephs-MacBook-Pro.local>
2025-03-31 21:34:55 -0600

    add a new student

--- dlt_pipeline.py
+++ dlt_pipeline.py
@@ -4,7 +4,7 @@ from dlt.sources.sql_database import sql_database
 def load_university_pipeline():
     source = sql_database().with_resources("courses", "course_assignments", "enrollments", "professors", "students", "student_admissions")
     
-    # source.professors.apply_hints(incremental=dlt.sources.incremental("created_at")) #incremental load
+    # source.students.apply_hints(incremental=dlt.sources.incremental("created_at")) #incremental load
     
     pipeline = dlt.pipeline(
         pipeline_name="university_data", #duckdb database name
